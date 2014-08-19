4e4dc7d63d5f05177171a8b958739e177fcf7f3a
Karlkim Suwanmongkol <contact@karlk.im>
2014-08-19 16:23:13 -0500

    Add angular-ui-bootstrap-jasmine sample

---
+++ vs_projects/Angular-UI-Bootstrap-Jasmine/Angular-UI-Bootstrap-Jasmine/ControllerSpec.js
@@ -0,0 +1,79 @@
+﻿describe('controllers', function() {
+    it('should pass', function() {
+        expect(1).toBe(1);
+    });
+
+    beforeEach(module('app'));
+
+    it('should load controller1', inject(function ($controller, $rootScope) {
+        var scope = $rootScope.$new();
+        var controller1 = $controller('controller1', { $scope: scope });
+
+        expect(controller1).toBeDefined();
+        expect(scope.items.length).toBe(3);        
+    }));
+
+
+    it('should work with $modal', inject(function ($controller, $rootScope) {
+        var scope = $rootScope.$new();
+
+        // mock result (promise)
+        var result = {
+            then: function (callback) {
+                callback('item1');
+            }
+        };
+
+        // mock $modal and $modal.open() then spy on it
+        var modal = {
+            open: function (options) {
+                expect(options.templateUrl).toBe('myModalContent.html');
+                expect(options.resolve.items().length).toBe(3);
+                return {
+                    result: result
+                };
+            }
+        };
+        spyOn(modal, "open")
+            .and
+            .callThrough();
+
+        var controller1 = $controller('controller1',
+           {
+               $scope: scope,
+               $modal: modal
+           });
+
+        scope.click();
+
+        expect(controller1).toBeDefined();
+        expect(scope.selected).toBe('item1');
+        expect(modal.open).toHaveBeenCalled();
+    }));
+
+    it('should work with $window', inject(function($controller, $rootScope) {
+        var scope = $rootScope.$new();
+        var window = {
+            location: {
+                href: "http://vgt.net"
+            }
+        };
+
+        var controller1 = $controller('controller1',
+           {
+               $scope: scope,
+               $window: window
+           });
+
+        spyOn(scope, 'setUrl')
+            .and
+            .callThrough();
+
+        expect(controller1).toBeDefined();
+        expect(scope.getUrl()).toBe("http://vgt.net");
+
+        scope.setUrl("http://news.vgt.net");
+        console.log(scope.setUrl.calls.allArgs());
+        expect(scope.getUrl()).toBe("http://news.vgt.net");
+    }));
+});
---
+++ vs_projects/Angular-UI-Bootstrap-Jasmine/Angular-UI-Bootstrap-Jasmine/controller.js
@@ -0,0 +1,51 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app');
+    app.controller('controller1', ['$scope', '$modal', '$window', function ($scope, $modal, $window) {
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.click = function (size) {
+            $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: modalInstanceCtrl,
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            }).result
+                .then(function (selectedItem) {
+                    $scope.selected = selectedItem;
+                });
+        };
+
+        $scope.getUrl = function() {
+            return $window.location.href;
+        };
+
+        $scope.setUrl = function(url) {
+            $window.location.href = url;
+            $scope.currentUrl = url;
+        };
+    }]);
+
+    var modalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
+
+   
+}());
