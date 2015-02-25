85cc91cfeb495decaf8b500773ed5a8e3701e52c
Karlkim Suwamongkol <contact@karlk.im>
2015-02-24 16:55:30 -0800

    running TypeScript tests with Chutzpah

---
+++ vs_projects/TestChutzpahTypeScript/TestChutzpahTypeScript/Scripts/mocha-tests-ts.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path="typings/mocha/mocha.d.ts" />
+/// <reference path="typings/chai/chai.d.ts" />
+
+var expect = chai.expect;
+describe('TypeScript Unit Tests:',() => {
+
+    describe('2 + 4',() => {
+        it('should be 6',(done) => {
+            expect(2 + 4).to.equals(6);
+            done();
+        });
+
+        it('should not be 7',(done) => {
+            expect(2 + 4).to.not.equals(7);
+            done();
+        });
+    });
+});
---
+++ vs_projects/TestChutzpahTypeScript/TestChutzpahTypeScript/Scripts/mocha-tests.js
@@ -0,0 +1,17 @@
+﻿var expect = chai.expect;
+describe('JavaScript Tests', function () {
+    describe('1 + 2 ', function () {
+        beforeEach(function () {
+        });
+        afterEach(function () {
+        });
+
+        it('should be 3', function () {
+            expect(1 + 2).to.equals(3);
+        });
+
+        it('should not be 4', function () {
+            expect(1 + 2).to.not.equals(4);
+        });
+    });
+});
