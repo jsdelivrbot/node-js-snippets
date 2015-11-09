2d248b62cbcf6781e9a876cc39649b92716e392f
Karlkim Suwamongkol <contact@karlk.im>
2015-11-08 21:31:52 -0800

    load css

--- try-webpack/index.js
+++ try-webpack/index.js
@@ -4,3 +4,13 @@ console.log(add(4,5));
 require(['./util-async.js'], function(addAsync){
   console.log(addAsync(1,2));
 })
+
+
+var css = require('style!css!./my.css');
+console.log(css);
+
+var $ = require('jquery');
+
+setTimeout(function(){
+  $('#h1Txt').css('color', 'red');
+}, 10000);
