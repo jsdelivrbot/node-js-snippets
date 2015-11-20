010168a52768149732a323e4d2529812c7df2466
Karlkim Suwamongkol <contact@karlk.im>
2015-11-20 10:20:15 -0800

    test 2048

--- selenium/2048.js
+++ selenium/2048.js
@@ -1,7 +1,15 @@
 var webdriver = require('selenium-webdriver'),
 	By = require('selenium-webdriver').By,
 	until = require('selenium-webdriver').until,
-	chrome = require('selenium-webdriver/chrome');
+	chrome = require('selenium-webdriver/chrome')
+  Key = require('selenium-webdriver').Key;
+
+var locators =
+  {
+    body : By.tagName('body'),
+    restart : By.css('a.restart-button'),
+    score: By.css('div.score-container')
+  };
 
 var browser = new webdriver
 					.Builder()
@@ -9,6 +17,26 @@ var browser = new webdriver
  					.build();
 
 browser.get('http://gabrielecirulli.github.io/2048/');
+
+function press(key){
+  browser
+    .findElement(locators.body)
+    .sendKeys(key);
+}
+
+function getScore(){
+  return browser.findElement(locators.score).getText();
+}
+
+setTimeout(function(){
+  press(Key.ARROW_UP);
+  press(Key.ARROW_DOWN);
+  press(Key.ARROW_LEFT);
+  press(Key.ARROW_RIGHT);
+  getScore().then(function(txt){
+    console.log(txt);
+  });
+}, 1000);
 
 setTimeout(function(){
 	browser.quit();
