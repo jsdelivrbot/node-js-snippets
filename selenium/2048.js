3eea4f03d13c52eb6986063190ba50b6ba45e719
Karlkim Suwamongkol <contact@karlk.im>
2015-11-20 10:58:06 -0800

    add tile position

--- selenium/2048.js
+++ selenium/2048.js
@@ -8,7 +8,8 @@ var locators =
   {
     body : By.tagName('body'),
     restart : By.css('a.restart-button'),
-    score: By.css('div.score-container')
+    score: By.css('div.score-container'),
+    tile: By.css('div.tile')
   };
 
 var browser = new webdriver
@@ -24,8 +25,17 @@ function press(key){
     .sendKeys(key);
 }
 
-function getScore(){
+function scorePromise(){
   return browser.findElement(locators.score).getText();
+}
+
+function tilesPromise(){
+  return browser.findElements(locators.tile);
+}
+
+function getTilePosition(tile){
+  // return tile.FindElement(By.xpath("..")).getAttribute('class');
+  return tile.getAttribute('class');
 }
 
 setTimeout(function(){
@@ -33,8 +43,21 @@ setTimeout(function(){
   press(Key.ARROW_DOWN);
   press(Key.ARROW_LEFT);
   press(Key.ARROW_RIGHT);
-  getScore().then(function(txt){
+  scorePromise().then(function(txt){
     console.log(txt);
+  });
+  browser.sleep(1000).then(function(){
+    tilesPromise().then(function(tiles){
+      for(var i = 0; i < tiles.length; i++){
+          tiles[i].getText().then(function(txt){
+            console.log(txt);
+          });
+
+          getTilePosition(tiles[i]).then(function(txt){
+            console.log(txt);
+          })
+      }
+    });
   });
 }, 1000);
 
