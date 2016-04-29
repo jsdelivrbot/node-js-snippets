da422fd6c41a3b01e0321c06bb05d5b7050c4dab
Karlkim Suwamongkol <contact@karlk.im>
2016-04-28 17:40:28 -0700

    vertical resize bar + refactoring

--- react/flex-dnd/app.js
+++ react/flex-dnd/app.js
@@ -9,46 +9,69 @@ require('jquery-ui/draggable');
 require('jquery-ui/droppable');
 
 class Pane extends React.Component {
+  componentDidMount(){
+    // $( `#${this.props.id}`).draggable({       
+    //   revert: true,
+    //   snap: true,
+    //   drag: () => {
+    //     let position = $( `#${this.props.id}`).position();
+    //     console.log(position);
+    //   }
+    // });  
+  }
+  
   render(){
-    const paneStyle = {
-      border: '1px solid black',
-      width: '100%',      
-      height: this.props.height
+    const paneStyle = {            
+      width: this.props.width,      
+      height: this.props.height,
+      marginLeft: this.props.marginLeft,
+      background: this.props.color,
     };
     
     return (
-      <div id={this.props.id} 
-        style={paneStyle}>PANE</div>
+      <div style={paneStyle}/>
     );
   }
 }
 
-class DragBar extends React.Component{
+class DragBar extends React.Component{  
   componentDidMount(){
-    $( `#${this.props.id}`).draggable({ 
-      axis: "y",
-      start: this.props.dragStart,
-      drag: () => {
-        let position = $( `#${this.props.id}`).position();
-        this.props.dragging(position);
-      },
-      stop: () => {
-        let position = $( `#${this.props.id}`).position();
-        this.props.dragStop(position);
-      }
-    });  
+    let elem = $(this.refs['drag-bar']);
+    elem
+      .draggable({ 
+        axis: this.props.axis,
+        cursor: "move",
+        start: this.props.dragStart,
+        drag: () => {
+          let position = elem.position();
+          this.props.dragging(position);
+        },
+        stop: () => {
+          let position = elem.position();
+          this.props.dragging(position);
+        }
+      });  
+  }
+  
+  mouseOver = () => {
+    this.refs['drag-bar'].style.cursor = 'move';    
   }
   
   render(){
     const dragBarStyle = { 
-      width: '100%',  
-      height: 5,
+      width: this.props.width,  
+      height: this.props.height,
       background: 'gray',
       position: 'absolute',
-      top: this.props.top
+      top: this.props.top,
+      left: this.props.left,
+      opacity: 0.0
     };
     return (
-      <div id={this.props.id} style={dragBarStyle}></div>
+      <div
+          ref='drag-bar'
+          onMouseOver={this.mouseOver} 
+          style={dragBarStyle}></div>
     );
   }
 }
@@ -59,52 +82,107 @@ class Container extends React.Component {
     super(props);
     
    let height = $(window).height() - 50;    
-
+   let width = $(window).width() - 50;
+   
     this.state = {
       height: height,
-      dragBar: {
-        id: 'drag-bar',
-        top: height/2
+      width: width,
+      map: {
+        id: 'map',
+        height: height,
+        width: width/2 
+      },
+      vDragBar: {
+        id: 'v-drag-bar',
+        top: 8,
+        left: width/2 + 10,
+        height: height,
+        width: 5,
+        axis: 'x'
+      },
+      hDragBar: {
+        id: 'h-drag-bar',
+        top: height/2 + 4, 
+        height: 6,
+        width: width/2,
+        axis: 'y'
       },
       pane1: {
         id: 'pane-1',
-        height: height/2 - 5
+        height: height/2,
+        width: width/2
       },
       pane2: {
         id: 'pane-2',
-        height: height/2 + 5
+        height: height/2,
+        width: width/2
       }  
     };   
   }
   
-  resize = (dragPosition) => {
+  hResize = (dragPosition) => {
+    let { height, width } = this.state;
     let height1 = dragPosition.top;
     let height2 = this.state.height - dragPosition.top;
-    
-    console.log('resize:', dragPosition, height1, height2);
-    
+        
     this.setState({
-      dragBar: {
-        id: 'drag-bar',
-        top: height1
+      map: {
+        height: this.state.map.height,
+        width: this.state.map.width
+      },
+      vDragBar: {
+        top: 10,
+        height: this.state.vDragBar.height,
+        width: 5
+      },
+      hDragBar: {
+        top: height1 + 4,
+        height: 6,
+        width: this.state.hDragBar.width
       },
       pane1: {
-        id: this.state.pane1.id,
-        height: height1 - 5
+        height: height1 - 3,
+        width: this.state.pane1.width
       },
       pane2: {
-        id: this.state.pane2.id,
-        height: height2 + 5
+        height: height2 + 3,
+        width: this.state.pane2.width
       }  
     });        
   }
   
+  vResize = (dragPosition) => {
+      let { height, width } = this.state;
+      let width1 = dragPosition.left - 5;
+      let width2 = width - dragPosition.left;
+        
+    this.setState({
+      map: {
+        height: this.state.map.height,
+        width: width1
+      },
+      vDragBar: {
+        top: 10,
+        height: this.state.vDragBar.height,
+        width: 5
+      },
+      hDragBar: {
+        top: this.state.hDragBar.top,
+        height: 5,
+        width: width2
+      },
+      pane1: {
+        height: this.state.pane1.height,
+        width: width2      
+      },
+      pane2: {
+        height: this.state.pane2.height,
+        width: width2
+      }  
+    });      
+  }
+  
   logPositions = () => {
-    let pane1 = $(`#${this.state.pane1.id}`);
-    let pane2 = $(`#${this.state.pane2.id}`);
-    let dragBar = $(`#${this.state.dragBar.id}`);
-    
-    console.log('logPositions:', pane1.height(), pane2.position().top, dragBar.position().top);
     console.log(this.state);
   }
   
@@ -121,21 +199,51 @@ class Container extends React.Component {
   render(){    
     const containerStyle = {
       display: 'flex',
-      flexDirection: 'column'
+      flexDirection: 'row'
+    };
+    
+    const rightContainerStyle = {
+      display: 'flex',
+      flexDirection: 'column'      
     };
     
     
     return (
       <div style={containerStyle}>
-        <Pane id={this.state.pane1.id} height={this.state.pane1.height}/>
+        <Pane height={this.state.map.height} 
+              width={this.state.map.width}
+              marginLeft={0}
+              color='cyan'/>
         <DragBar 
-          id={this.state.dragBar.id}
-          top={this.state.dragBar.top}
-          dragStart={this.logPositions}
-          dragging={this.resize}
-          dragStop={this.logPositions}
+            top={this.state.vDragBar.top}
+            height={this.state.vDragBar.height}
+            width={this.state.vDragBar.width}
+            dragStart={this.logPositions}
+            dragging={this.vResize}
+            dragStop={this.logPositions}
+            axis={this.state.vDragBar.axis}
+            left={this.state.vDragBar.left}
         />
-        <Pane id={this.state.pane2.id} height={this.state.pane2.height}/>
+        <div style={rightContainerStyle}>
+          <Pane height={this.state.pane1.height} 
+                width={this.state.pane1.width}
+                marginLeft={0}
+                color='fuchsia'/>
+          <DragBar 
+            top={this.state.hDragBar.top}
+            height={this.state.hDragBar.height}
+            width={this.state.hDragBar.width}
+            dragStart={this.logPositions}
+            dragging={this.hResize}
+            dragStop={this.logPositions}
+            axis={this.state.hDragBar.axis}
+            marginLeft={0}
+          />
+          <Pane height={this.state.pane2.height} 
+                width={this.state.pane2.width}
+                marginLeft={0}
+                color='blue'/>
+        </div>
       </div>
     );
   }
