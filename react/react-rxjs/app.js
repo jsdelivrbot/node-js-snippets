69ddc5247fa39691fb1d126bfa957cd6b2e99f3e
Karlkim Suwamongkol <contact@karlk.im>
2016-07-07 02:38:21 -0700

    interval & fromEvent

--- react/react-rxjs/app.js
+++ react/react-rxjs/app.js
@@ -2,27 +2,25 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 var Rx = require('@reactivex/rxjs');
 
-let count = Rx.Observable.create((observer) => {
-  observer.next(0);
-});
-
-
-count.subscribe((x) => {
-  console.log('x:', x);
-})
-
+let count = Rx.Observable.interval(1000);
 let num = 0;
 
-window.increase = function(){
+count.subscribe(() => {
   num++;
-}
+  ReactDOM.render(<App num={num}/>, document.getElementById('react'));
+})
+
+let reset = Rx.Observable.fromEvent(document, 'click');
+reset.subscribe(() => {
+  num = 0;
+  ReactDOM.render(<App num={num}/>, document.getElementById('react'));
+});
 
 class App extends React.Component{
   render(){
     return (
-      <div>{num}</div>
+      <div>{this.props.num}</div>
     );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('react'));
