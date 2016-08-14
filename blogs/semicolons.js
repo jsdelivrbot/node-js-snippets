1b175d3129b8d7e471228393f470f2ff014566d5
Karlkim Suwamongkol <contact@karlk.im>
2016-08-15 06:13:13 +0700

    semicolons

---
+++ blogs/semicolons.js
@@ -0,0 +1,29 @@
+// without semicolon, fun is executed (iffy)
+// with the value in parenthesis
+
+var fun = function(input){
+  input = input || 0
+  return input
+} // no semicolon
+
+(function(){
+  var var12345 = 10
+  var var23456 = 10
+  var var34567
+  var34567 =  var12345 + var23456
+  return var34567
+})
+
+console.log(fun()); // actual 20, expect 0
+
+var fun2 = function(x){
+  return x;
+}
+(9)
+
+console.log(fun2); // 20
+console.log(fun2()); // ERROR: fun2 is not a function
+
+// How babel and uglifyjs works
+// ./node_modules/.bin/babel semicolons.js
+// ./node_modules/.bin/uglifyjs  semicolons.js -m -c
