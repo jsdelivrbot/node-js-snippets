27805ef9fc6e4a34fff7c9ae37f64c5dd7027ac6
Karlkim Suwamongkol <contact@karlk.im>
2017-01-17 11:34:41 -0800

    diceRoll with argument

--- graphql/graphql-project/schema/main.js
+++ graphql/graphql-project/schema/main.js
@@ -17,7 +17,16 @@ const queryType = new GraphQLObjectType({
     },
     diceRoll: {
       type: new GraphQLList(GraphQLInt),
-      resolve: () => [roll(), roll()]
+      args: {
+        count: { type: GraphQLInt }
+      },
+      resolve: (_, args) => {
+        let rolls = []
+        for (let i = 0; i < args.count; i++) {
+          rolls.push(roll())
+        }
+        return rolls
+      }
     }
   }
 })
